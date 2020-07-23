5083f4dd3cb7bbd97bcc1a2248435cbb38ad14ee
nikkokun <mizutaninikkou@gmail.com>
2020-07-23 19:20:31 +0900

    Fix sort, add automated testing

--- src/main.cu
+++ src/main.cu
@@ -4,26 +4,27 @@
 #include <vector>
 #include <thrust/host_vector.h>
 #include <thrust/device_vector.h>
-#include <thrust/sort.h>
 #include <thrust/execution_policy.h>
 #include <random>
-#include <chrono>
+#include <fstream>
+#include <string>
 
-
-#define readset_size 20
-#define transaction_size 4000000
-
-const int table_size = 1000000;
-const int THREADS = 10000;
-const int BLOCKS = 100;
-const int STRIDE = transaction_size / THREADS;
-typedef int transactions[readset_size];
 
 using namespace thrust;
 using namespace std;
 
-__device__ int lock(int *lock) {
+#define NUM_OPERATIONS 20
+#define NUM_TRANSACTIONS 4000000
+#define NUM_RECORDS 1000000
+typedef int transactions[NUM_OPERATIONS];
 
+//1, 5, 10, 50, 100, 250, 500, 1000, 2500, 5000, 10000
+const int THREADS = 5000;
+const int BLOCKS = 10;
+const int STRIDE = NUM_TRANSACTIONS / THREADS;
+const int NUM_RUNS = 10;
+
+__device__ int lock(int *lock) {
     return atomicCAS(lock, 0, 1);
 }
 
@@ -31,56 +32,13 @@ __device__ void unlock(int *lock) {
     atomicExch(lock, 0);
 }
 
-__device__ void swap(int* a, int* b)
-{
-    int t = *a;
-    *a = *b;
-    *b = t;
-}
-
-__device__ int partition (int arr[], int low, int high)
-{
-    int pivot = arr[high]; // pivot
-    int i = (low - 1); // Index of smaller element
-
-    for (int j = low; j <= high - 1; j++)
-    {
-        // If current element is smaller than the pivot
-        if (arr[j] < pivot)
-        {
-            i++; // increment index of smaller element
-            swap(&arr[i], &arr[j]);
-        }
-    }
-    swap(&arr[i + 1], &arr[high]);
-    return (i + 1);
-}
-
-__device__ void quickSort(int arr[], int low, int high)
-{
-    if (low < high)
-    {
-        /* pi is partitioning index, arr[p] is now
-        at right place */
-        int pi = partition(arr, low, high);
-
-        // Separately sort elements before
-        // partition and after partition
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
-    }
-}
-
-__device__ void insertionSort(int arr[])
+__device__ void insertion_sort(int *arr)
 {
     int i, key, j;
-    for (i = 1; i < readset_size; i++) {
+    for (i = 1; i < NUM_OPERATIONS; i++) {
         key = arr[i];
         j = i - 1;
 
-        /* Move elements of arr[0..i-1], that are
-          greater than key, to one position ahead
-          of their current position */
         while (j >= 0 && arr[j] > key) {
             arr[j + 1] = arr[j];
             j = j - 1;
@@ -89,7 +47,7 @@ __device__ void insertionSort(int arr[])
     }
 }
 
-__global__ void myKernel(int *lock_table, int *table, transactions *d_transactions, int stride) {
+__global__ void transaction_worker(int *lock_table, int *table, transactions *d_transactions, int stride) {
 
     int tid =  blockIdx.x *blockDim.x + threadIdx.x;
     int start = tid * stride;
@@ -97,19 +55,17 @@ __global__ void myKernel(int *lock_table, int *table, transactions *d_transactio
 
 //    sort phase
     for(int i = start; i < end; ++i) {
-//        quickSort(d_transactions[i], 0, readset_size-1);
-        insertionSort(d_transactions[i]);
+        insertion_sort(d_transactions[i]);
     }
     //process transactions
     for(int i = start; i < end; ++i) {
         //growing phase
         int prev = -1;
-        int abort_index[readset_size];
+        int abort_index[NUM_OPERATIONS];
         memset(abort_index, -1, sizeof(abort_index));
         int is_abort = 0;
 
-
-        for(int j = 0; j< readset_size; ++j) {
+        for(int j = 0; j < NUM_OPERATIONS; ++j) {
             int idx = d_transactions[i][j];
             if(idx != prev) {
                 int is_locked = lock(&lock_table[idx]);
@@ -126,7 +82,7 @@ __global__ void myKernel(int *lock_table, int *table, transactions *d_transactio
         if(is_abort == 0) {
             //critical section
             prev = -1;
-            for(int j = 0; j < readset_size; ++j) {
+            for(int j = 0; j < NUM_OPERATIONS; ++j) {
                 int idx = d_transactions[i][j];
                 if(idx != prev) {
                     table[idx] += 1;
@@ -136,7 +92,7 @@ __global__ void myKernel(int *lock_table, int *table, transactions *d_transactio
 
             //shrinking phase
             prev = -1;
-            for(int j = 0; j < readset_size; ++j) {
+            for(int j = 0; j < NUM_OPERATIONS; ++j) {
                 int idx = d_transactions[i][j];
                 if(idx != prev) {
                     unlock(&lock_table[idx]);
@@ -144,7 +100,7 @@ __global__ void myKernel(int *lock_table, int *table, transactions *d_transactio
                 prev = idx;
             }
         } else {
-            for(int j = 0; j < readset_size; ++j) {
+            for(int j = 0; j < NUM_OPERATIONS; ++j) {
                 int idx = abort_index[j];
                 if(idx != -1) {
                     unlock(&lock_table[idx]);
@@ -156,76 +112,78 @@ __global__ void myKernel(int *lock_table, int *table, transactions *d_transactio
 }
 
 int main() {
-    transactions *h_transactions;
-    transactions *d_transactions;
+    string OUTPUT_FP = "/home/nicoroble/cuda-s2pl/data/cuda-transactions_readsetsize-" + to_string(NUM_OPERATIONS) + "_transactions-" + to_string(NUM_TRANSACTIONS) + "_tablesize-" + to_string(NUM_RECORDS) + ".tsv";
 
-    size_t dsize = transaction_size*readset_size*sizeof(int);
+    vector<float> runtimes;
 
-    int *lock_table;
-    cudaMalloc((void**)&lock_table, table_size * sizeof(int));
-    cudaMemset(lock_table, 0, table_size * sizeof(int));
+    for(int n = 0; n < NUM_RUNS; ++n) {
+        transactions *h_transactions;
+        transactions *d_transactions;
+        int *lock_table;
+        int *table;
+        int *d_table;
 
-    int *table;
-    int *d_table;
+        size_t dsize = NUM_TRANSACTIONS * NUM_OPERATIONS * sizeof(int);
+        cudaMalloc((void**)&lock_table, NUM_RECORDS * sizeof(int));
+        cudaMemset(lock_table, 0, NUM_RECORDS * sizeof(int));
 
-    table = (int*)malloc(sizeof(int) * table_size);
-    for(int i =0; i < table_size; ++i) {
-        table[i] = 0;
-    }
-
-
-    cudaMalloc((void**)&d_table, sizeof(int) * table_size);
-    cudaMemcpy(d_table, table, sizeof(int) * table_size, cudaMemcpyHostToDevice);
-
-
-    //set memory for host arrays
-    h_transactions = (transactions *)malloc(dsize);
-    // populate h_ arrays
-    memset(h_transactions, 0, dsize);
-
-    random_device rd;
-    mt19937 eng(rd());
-    uniform_int_distribution<> distribution(0, table_size - 1);
-
-    for(int i = 0; i < transaction_size; ++i) {
-        for(int j =0; j < readset_size; ++j) {
-            h_transactions[i][j] = distribution(eng);
+        table = (int*)malloc(sizeof(int) * NUM_RECORDS);
+        for(int i =0; i < NUM_RECORDS; ++i) {
+            table[i] = 0;
         }
+
+
+        cudaMalloc((void**)&d_table, sizeof(int) * NUM_RECORDS);
+        cudaMemcpy(d_table, table, sizeof(int) * NUM_RECORDS, cudaMemcpyHostToDevice);
+
+        //set memory for host arrays
+        h_transactions = (transactions *)malloc(dsize);
+        // populate h_ arrays
+        memset(h_transactions, 0, dsize);
+
+        random_device rd;
+        mt19937 eng(rd());
+        uniform_int_distribution<> distribution(0, NUM_RECORDS - 1);
+
+        for(int i = 0; i < NUM_TRANSACTIONS; ++i) {
+            for(int j =0; j < NUM_OPERATIONS; ++j) {
+                h_transactions[i][j] = distribution(eng);
+            }
+        }
+
+        // Allocate memory on device
+        cudaMalloc(&d_transactions, dsize);
+
+        // Do memcopies to GPU
+        cudaMemcpy(d_transactions, h_transactions, dsize, cudaMemcpyHostToDevice);
+
+        float elapsed=0;
+        cudaEvent_t start, stop;
+        cudaEventCreate(&start);
+        cudaEventCreate(&stop);
+
+        cudaEventRecord(start, 0);
+
+        transaction_worker<<<THREADS / BLOCKS, BLOCKS>>>(lock_table, d_table, d_transactions, STRIDE);
+
+        cudaEventRecord(stop, 0);
+        cudaEventSynchronize(stop);
+        cudaEventElapsedTime(&elapsed, start, stop);
+
+        runtimes.push_back(elapsed);
+
+        free(h_transactions);
+        cudaFree(d_transactions);
+        cudaFree(d_table);
+        cudaFree(lock_table);
+        cudaDeviceSynchronize();
     }
 
-
-    // Allocate memory on device
-    cudaMalloc(&d_transactions, dsize);
-
-    // Do memcopies to GPU
-    cudaMemcpy(d_transactions, h_transactions, dsize, cudaMemcpyHostToDevice);
-
-    float elapsed=0;
-    cudaEvent_t start, stop;
-    cudaEventCreate(&start);
-    cudaEventCreate(&stop);
-
-    cout << "starting...\n";
-
-    cudaEventRecord(start, 0);
-
-    myKernel<<<THREADS/BLOCKS,BLOCKS>>>(lock_table, d_table, d_transactions, STRIDE);
-    cudaEventRecord(stop, 0);
-    cudaEventSynchronize(stop);
-    cudaEventElapsedTime(&elapsed, start, stop);
-
-    cout << "Finished after " << elapsed << " milliseconds";
-
-
-
-    // Do memcopies back to host
-    cudaMemcpy(h_transactions, d_transactions, dsize, cudaMemcpyDeviceToHost);
-    cudaMemcpy(table, d_table, sizeof(int) * table_size, cudaMemcpyDeviceToHost);
-
-
-    cudaFree(d_transactions);
-    cudaFree(d_table);
-
+    ofstream outfile;
+    outfile.open(OUTPUT_FP, ios_base::app);
+    float average_runtime = accumulate(runtimes.begin(), runtimes.end(), 0.0) / runtimes.size();
+    outfile << THREADS << "\t" << average_runtime << "\t" << runtimes.size() << "\n";
+    outfile.close();
 
     return 0;
 }
